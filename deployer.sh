356661aa14d92643b9488328f6a314300990db28
Bizley <pawel@positive.codes>
2016-08-16 12:13:39 +0200

    v1.2

--- deployer.sh
+++ deployer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Human Device Yii 2 deployer v1.1
+# Human Device Yii 2 deployer v1.2
 # ========================================================
 # -h, --help for help
 
@@ -9,22 +9,31 @@
 # project host folder name
 HOST="change me"
 
-# default environment name for init
+# default environment name for Yii 2 init
 ENV="Development"
 
 # SVN credentials
-SVN_URL="change me"
-SVN_USER="change me"
+SVN_URL='change me'
+SVN_USER='change me'
 SVN_PASS='change me'
 
-# project name
+# SVN branch path for development release
+SVN_BRANCH='branches/dev'
+
+# folders owner and group if needed (use chown syntax)
+RIGHTS=""
+
+# project name (just for display)
 PROJECT="change me"
 
-# releases folder name
+# production releases folder name
 R_FOLDER="releases"
 
-# development release folder name
-D_FOLDER="_dev_"
+# development current release folder name
+DC_FOLDER="dev_current"
+
+# development next release folder name
+DN_FOLDER="dev_next"
 
 # composer folder name for vendor storage
 C_FOLDER="composer"
@@ -40,11 +49,13 @@ USING_TEMPORARY_COMPOSER=0
 MODE=0
 COMPOSER_DONT_UPDATE=0
 
+# generate line with dots
 LINE() {
     local LINE=$(printf '%0.1s' "."{1..60})
     printf "%s%s" "$1" "${LINE:${#1}}"
 }
 
+# cleans temporary folder
 END_MARKER() {
     if [[ $USING_TEMPORARY_COMPOSER -eq 1 ]]; then
         LINE " > deleting temporary composer folder"
@@ -56,17 +67,22 @@ END_MARKER() {
     echo "FINISHED at $(date +"%r")."
 }
 
+# creates folder
 CREATE_FOLDER() {
     LINE "$1"
     if mkdir "$2"
     then
         chmod 0775 "$2"
+        if [[ "$RIGHTS" != "" ]]; then
+            chown ${RIGHTS} "$2"
+        fi
         echo "created"
         return 1
     fi
     return 0
 }
 
+# initialises release folder
 INIT_RELEASE_FOLDER() {
     if [[ ! -d "./${R_FOLDER}"  ]]; then
         CREATE_FOLDER " > initialing releases folder" "./${R_FOLDER}"
@@ -84,27 +100,27 @@ INIT_RELEASE_FOLDER() {
     return 0
 }
 
-INIT_DEV_RELEASE_FOLDER() {
+# initialises development folder
+INIT_DEVELOPMENT_FOLDER() {
     if [[ ! -d "./${R_FOLDER}"  ]]; then
         CREATE_FOLDER " > initialing releases folder" "./${R_FOLDER}"
         if [[ $? -eq 0 ]]; then
             return 0
         fi
     fi
-    LINE " > removing DEV release folder"
-    if [[ -d "./${R_FOLDER}/${D_FOLDER}" ]]; then
-        if rm -rf "./${R_FOLDER}/${D_FOLDER}"
+    if [[ -d "./${R_FOLDER}/${DN_FOLDER}" ]]; then
+        LINE " > deleting old next development release folder"
+        if rm -rf "./${R_FOLDER}/${DN_FOLDER}"
         then
             echo "deleted"
-            return 1
+        else
+            return 0
         fi
-    else
-        echo "ready"
-        return 1
     fi
-    return 0
+    return 1
 }
 
+# checks release folder
 CHECK_RELEASE_FOLDER() {
     LINE " > checking release ${1} folder"
     if [[ ! -d "./${R_FOLDER}/$1" ]]; then
@@ -116,7 +132,8 @@ CHECK_RELEASE_FOLDER() {
     return 0
 }
 
-DOWNLOAD_RELEASE() {
+# downloads production release SVN folder
+DOWNLOAD_PROD_RELEASE() {
     LINE " > downloading SVN snapshot"
     local CMD="svn export -q \"${SVN_URL}/tags/${1}\" \"./${R_FOLDER}/${1}\" --no-auth-cache --non-interactive --trust-server-cert --username \"${SVN_USER}\" --password '${SVN_PASS}'"
     if [[ $VERBOSE -eq 1 ]]; then
@@ -125,25 +142,33 @@ DOWNLOAD_RELEASE() {
     if eval "$CMD"
     then
         echo "downloaded"
+        if [[ "$RIGHTS" != "" ]]; then
+            chown -R ${RIGHTS} "./${R_FOLDER}/${1}"
+        fi
         return 1
     fi
     return 0
 }
 
+# downloads development release SVN folder
 DOWNLOAD_DEV_RELEASE() {
-    LINE " > downloading DEV SVN snapshot"
-    local CMD="svn export -q \"${SVN_URL}/branches/dev\" \"./${R_FOLDER}/${D_FOLDER}\" --no-auth-cache --non-interactive --trust-server-cert --username \"${SVN_USER}\" --password '${SVN_PASS}'"
+    LINE " > downloading development SVN snapshot"
+    local CMD="svn export -q \"${SVN_URL}/${SVN_BRANCH}\" \"./${R_FOLDER}/${DN_FOLDER}\" --no-auth-cache --non-interactive --trust-server-cert --username \"${SVN_USER}\" --password '${SVN_PASS}'"
     if [[ $VERBOSE -eq 1 ]]; then
-        CMD="svn export \"${SVN_URL}/branches/dev\" \"./${R_FOLDER}/${D_FOLDER}\" --no-auth-cache --non-interactive --trust-server-cert --username \"${SVN_USER}\" --password '${SVN_PASS}'"
+        CMD="svn export \"${SVN_URL}/${SVN_BRANCH}\" \"./${R_FOLDER}/${DN_FOLDER}\" --no-auth-cache --non-interactive --trust-server-cert --username \"${SVN_USER}\" --password '${SVN_PASS}'"
     fi
     if eval "$CMD"
     then
         echo "downloaded"
+        if [[ "$RIGHTS" != "" ]]; then
+            chown -R ${RIGHTS} "./${R_FOLDER}/${DN_FOLDER}"
+        fi
         return 1
     fi
     return 0
 }
 
+# installs composer dependencies
 COMPOSER_INSTALL() {
     LINE " > installing composer dependencies"
     if [[ $USING_TEMPORARY_COMPOSER -eq 1 ]]; then
@@ -165,6 +190,7 @@ COMPOSER_INSTALL() {
     return 0
 }
 
+# updates composer dependencies
 COMPOSER_UPDATE() {
     if [[ $COMPOSER_DONT_UPDATE -eq 1 ]]; then
         return 1
@@ -185,6 +211,7 @@ COMPOSER_UPDATE() {
     return 0
 }
 
+# checks vendor folder
 CHECK_VENDOR() {
     if [[ ! -d "./${C_FOLDER}" ]]; then
         CREATE_FOLDER " > creating composer folder" "./${C_FOLDER}"
@@ -241,6 +268,7 @@ CHECK_VENDOR() {
     return 0
 }
 
+# symlinks folders
 SYMLINK() {
     LINE " > symlinking vendor folder to release $1"
     local CMD="ln -s \"./../../${C_FOLDER}/vendor\" \"./${R_FOLDER}/${1}/vendor\""
@@ -255,6 +283,7 @@ SYMLINK() {
     return 0
 }
 
+# starts Yii 2 init
 INIT() {
     LINE " > running Yii 2 init for ${ENV}"
     local CMD="php \"./${R_FOLDER}/${1}/init\" --env=${ENV} --overwrite=All >/dev/null"
@@ -269,6 +298,7 @@ INIT() {
     return 0
 }
 
+# switches temporary composer folder
 SWITCH_TEMP_COMPOSER() {
     if [[ $USING_TEMPORARY_COMPOSER -eq 1 ]]; then
         if rm "./${R_FOLDER}/${1}/vendor"
@@ -290,6 +320,7 @@ SWITCH_TEMP_COMPOSER() {
     return 0
 }
 
+# symlinks release
 SYMLINK_RELEASE() {
     SWITCH_TEMP_COMPOSER "$1"
     if [[ $? -eq 1 ]]; then
@@ -302,6 +333,7 @@ SYMLINK_RELEASE() {
     return 0
 }
 
+# creates backup
 BACKUP_CREATE() {
     LINE " > creating host symlink and vendor backup"
     if cp -a "./${HOST}" "./${HOST}_backup"
@@ -315,6 +347,7 @@ BACKUP_CREATE() {
     return 0
 }
 
+# deletes backup
 BACKUP_DELETE() {
     LINE " > deleting host symlink and vendor backup"
     if rm "./${HOST}_backup"
@@ -326,6 +359,7 @@ BACKUP_DELETE() {
     fi
 }
 
+# recovers backup
 BACKUP_RECOVER() {
     LINE " > recovering host symlink and vendor from backup"
     if [[ -d "./${C_FOLDER}/vendor" ]]; then
@@ -340,6 +374,7 @@ BACKUP_RECOVER() {
     fi
 }
 
+# switches production releases
 SWITCH() {
     if [[ ! -h "./${HOST}" ]]; then
         LINE " > symlinking ${HOST} to release $1"
@@ -367,11 +402,57 @@ SWITCH() {
     return 0
 }
 
+# switches development releases
+SWITCHDEV() {
+    if [[ -d "./${R_FOLDER}/${DC_FOLDER}" ]]; then
+        LINE " > removing old current development release"
+        if rm -rf "./${R_FOLDER}/${DC_FOLDER}"
+        then
+            echo "done"
+        else
+            return 0
+        fi
+    fi
+    LINE " > moving next development release to current"
+    if cp -a "./${R_FOLDER}/${DN_FOLDER}" "./${R_FOLDER}/${DC_FOLDER}"
+    then
+        echo "done"
+        rm -rf "./${R_FOLDER}/${DN_FOLDER}"
+    else
+        return 0
+    fi
+    if [[ ! -h "./${HOST}" ]]; then
+        LINE " > symlinking ${HOST} to development release"
+        SYMLINK_RELEASE "${DC_FOLDER}"
+        if [[ $? -eq 1 ]]; then
+            return 1
+        fi
+    else
+        BACKUP_CREATE
+        if [[ $? -eq 1 ]]; then
+            LINE " > switching to development release"
+            if rm "./${HOST}"
+            then
+                SYMLINK_RELEASE "${DC_FOLDER}"
+                if [[ $? -eq 1 ]]; then
+                    BACKUP_DELETE
+                    return 1
+                else
+                    BACKUP_RECOVER
+                fi
+            fi
+            BACKUP_DELETE
+        fi
+    fi
+    return 0
+}
+
+# deploys tag
 DEPLOY() {
     echo "STARTING deployment of ${PROJECT} version ${VERSION} at $(date +"%r")."
     INIT_RELEASE_FOLDER "$VERSION"
     if [[ $? -eq 1 ]]; then
-        DOWNLOAD_RELEASE "$VERSION"
+        DOWNLOAD_PROD_RELEASE "$VERSION"
         if [[ $? -eq 1 ]]; then
             CHECK_VENDOR "$VERSION"
             if [[ $? -eq 1 ]]; then
@@ -388,6 +469,7 @@ DEPLOY() {
     END_MARKER
 }
 
+# rollbacks tag
 ROLLBACK() {
     echo "STARTING rollback of ${PROJECT} to version ${VERSION} at $(date +"%r")."
     CHECK_RELEASE_FOLDER "$VERSION"
@@ -403,19 +485,20 @@ ROLLBACK() {
     END_MARKER
 }
 
+# deploys development
 DEPLOYDEV() {
-    echo "STARTING deployment of ${PROJECT} version DEV at $(date +"%r")."
-    INIT_DEV_RELEASE_FOLDER
+    echo "STARTING deployment of ${PROJECT} development version at $(date +"%r")."
+    INIT_DEVELOPMENT_FOLDER
     if [[ $? -eq 1 ]]; then
         DOWNLOAD_DEV_RELEASE
         if [[ $? -eq 1 ]]; then
-            CHECK_VENDOR "$D_FOLDER"
+            CHECK_VENDOR "$DN_FOLDER"
             if [[ $? -eq 1 ]]; then
-                SYMLINK "$D_FOLDER"
+                SYMLINK "$DN_FOLDER"
                 if [[ $? -eq 1 ]]; then
-                    INIT "$D_FOLDER"
+                    INIT "$DN_FOLDER"
                     if [[ $? -eq 1 ]]; then
-                        SWITCH "$D_FOLDER"
+                        SWITCHDEV
                     fi
                 fi
             fi
@@ -424,6 +507,7 @@ DEPLOYDEV() {
     END_MARKER
 }
 
+# starts script
 START() {
     if [[ $MODE -eq 1 ]]; then
         echo "NAME"
@@ -452,7 +536,7 @@ START() {
         echo "        rollback to TAG version"
         echo ""
         echo "    -dev"
-        echo "        deploy DEV version"
+        echo "        deploy development version"
         echo ""
         echo "    -v, --verbose"
         echo "        runs the script in verbose mode where output of svn, composer and init is visible"
@@ -549,7 +633,6 @@ do
             VERSION="${2##*/}"
             shift
             ;;
-        
         *)
             MODE=6
             break
